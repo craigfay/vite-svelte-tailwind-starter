72b38131ffdcce6cbc1047a8e6470931a2c3a688
craigfay <craigfay1@gmail.com>
2022-04-22 00:13:49 -0500

    Initial Commit

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+
+import tailwind from 'tailwindcss'
+import tailwindConfig from './tailwind.config.cjs'
+import autoprefixer from 'autoprefixer'
+
+export default {
+  plugins:[
+    tailwind(tailwindConfig),
+    autoprefixer
+  ]
+}
---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<style global>
+  @tailwind utilities;
+  @tailwind components;
+  @tailwind base;
+</style>
+
+<h1 class="text-4xl font-bold m-5">Hello world!</h1>
